94680fc15cb0a50a2e2266803368a4c865d39b44
nyanyacyan <nyanyacyan@gmail.com>
2023-10-04 09:00:11 +0900

    スプレッドシートへの書き込むための関数作成 スクレイピングするための箱作成

--- SpreadsheetAppender.py
+++ SpreadsheetAppender.py
@@ -1,20 +1,21 @@
 import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import scraper
 
-def store_in_spreadsheet(data):
+def sheet_append_list(sheet, data):
+    sheet.append_row([datetime.datetime.now().strftime('%Y-%m-%d')] + data)
+
+def result_in_spreadsheet(data):
     # クレデンシャルのセットアップ
     scope = ['https://www.googleapis.com/auth/spreadsheets']
     creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_credentials.json', scope)
     client = gspread.authorize(creds)
 
     # スプレッドシートを開く
-    sheet = client.open("1wGhuvB2Gs1btoq2DJlkdMlCixdvZdEazvYyOQm2T6EE").sheet1
+    spreadsheet = client.open("1wGhuvB2Gs1btoq2DJlkdMlCixdvZdEazvYyOQm2T6EE")
 
-    # 最後の行にデータを追加
-    next_row = len(sheet.col_values(1)) + 1  # 1列目の長さを取得して次の行を決定
-    sheet.append_row([datetime.datetime.now().strftime('%Y-%m-%d')] + data)
-
-# スクレイピングのロジック（ここは適宜修正してください）
-data_scraped = ["sample_data1", "sample_data2", "sample_data3"]
-store_in_spreadsheet(data_scraped)
+    # それぞれのワークシートにデータを追加
+    sheet_append_list(spreadsheet.worksheet("Sneaker"), scraper.sneaker_data)
+    sheet_append_list(spreadsheet.worksheet("Gems"), scraper.gems_data)
+    sheet_append_list(spreadsheet.worksheet("Scroll"), scraper.scroll_data)
--- scraper.py
+++ scraper.py
@@ -1,1 +1,15 @@
 import requests
+from selenium import webdriver
+
+url = "https://stepn-market.guide/market/dashboard"
+scraparsite = requests.get(url)
+
+# 応答のステータスコードが200（成功）の場合、内容を表示
+if scraparsite.status_code == 200:
+    print(scraparsite.text)
+else:
+    print(f"Error {scraparsite.status_code}: Failed to fetch the webpage.")
+
+sneaker_data = []
+gems_data = []
+scroll_data = []
