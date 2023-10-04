48c0e7eca4b1cd7206d420a6204babb53e4afb1e
nyanyacyan <nyanyacyan@gmail.com>
2023-10-05 07:39:54 +0900

    spreadsheet追記できるように実装 スクレイプする部分の修正

--- SpreadsheetAppender.py
+++ SpreadsheetAppender.py
@@ -3,19 +3,54 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import scraper
 
-def sheet_append_list(sheet, data):
-    sheet.append_row([datetime.datetime.now().strftime('%Y-%m-%d')] + data)
-
-def result_in_spreadsheet(data):
+def get_spreadsheet(data):
     # クレデンシャルのセットアップ
     scope = ['https://www.googleapis.com/auth/spreadsheets']
     creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_credentials.json', scope)
     client = gspread.authorize(creds)
+    return client.open("1wGhuvB2Gs1btoq2DJlkdMlCixdvZdEazvYyOQm2T6EE")
 
-    # スプレッドシートを開く
-    spreadsheet = client.open("1wGhuvB2Gs1btoq2DJlkdMlCixdvZdEazvYyOQm2T6EE")
+# spreadsheetのAから追記していく
+def append_from_A(sheet, data):
+    sheet.append_row([datetime.datetime.now().strftime('%Y-%m-%d')] + data)
 
-    # それぞれのワークシートにデータを追加
-    sheet_append_list(spreadsheet.worksheet("Sneaker"), scraper.sneaker_data)
-    sheet_append_list(spreadsheet.worksheet("Gems"), scraper.gems_data)
-    sheet_append_list(spreadsheet.worksheet("Scroll"), scraper.scroll_data)
+# spreadsheetのHから追記していく
+def append_from_H(sheet, data):
+    # H列の空でないセルの数を取得して、次の行を決定
+    w_calum_start = len(sheet.col_values(7)) + 1  # 23はW列のインデックス
+    
+    # 日付とデータをW列から書き込む
+    sheet.update_cell(w_calum_start, 7, datetime.datetime.now().strftime('%Y-%m-%d'))  # 7はH列
+    for i, value in enumerate(data, start=1):
+        sheet.update_cell(w_calum_start, 7 + i, value)
+
+# spreadsheetのWから追記していく
+def append_from_W(sheet, data):
+    # W列の空でないセルの数を取得して、次の行を決定
+    w_calum_start = len(sheet.col_values(23)) + 1  # 23はW列のインデックス
+    
+    # 日付とデータをW列から書き込む
+    sheet.update_cell(w_calum_start, 23, datetime.datetime.now().strftime('%Y-%m-%d'))  # 23はW列
+    for i, value in enumerate(data, start=1):
+        sheet.update_cell(w_calum_start, 23 + i, value)
+
+# spreadsheetのAVから追記していく
+def append_from_AV(sheet, data):
+    # W列の空でないセルの数を取得して、次の行を決定
+    av_calum_start = len(sheet.col_values(74)) + 1  # 74はAV列のインデックス
+    
+    # 日付とデータをW列から書き込む
+    sheet.update_cell(av_calum_start, 74, datetime.datetime.now().strftime('%Y-%m-%d'))  # 74はW列
+    for i, value in enumerate(data, start=1):
+        sheet.update_cell(av_calum_start, 74 + i, value)
+
+
+# それぞれのワークシートにデータを追加
+def result_in_spreadsheet():
+    spreadsheet = get_spreadsheet()
+    append_from_A(spreadsheet.worksheet("Sneaker"), scraper.sneaker_data)
+    append_from_W(spreadsheet.worksheet("Sneaker"), scraper.sneaker_count_data)
+    append_from_A(spreadsheet.worksheet("Gems"), scraper.gems_data)
+    append_from_AV(spreadsheet.worksheet("Gems"), scraper.gems_count_data)
+    append_from_A(spreadsheet.worksheet("Scroll"), scraper.scroll_data)
+    append_from_H(spreadsheet.worksheet("Scroll"), scraper.scroll_count_data)
--- scraper.py
+++ scraper.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 
 url = "https://stepn-market.guide/market/dashboard"
 
-driver = webdriver.Chrome(executable_path=)
+driver = webdriver.Chrome(executable_path=)  # 修正必要
 
 # URLを開く
 driver.get(url)
 
 # <b>タグのテキストを取得
 all_btag_list = [b_tag.text for b_tag in driver.find_elements_by_tag_name('b')]
+print(all_btag_list)
 
 # 数字のみ、GMTは消去。
 def string_intcleaning(s):
@@ -16,10 +17,16 @@ def string_intcleaning(s):
     return int(cleaned)
 
 # それぞれの範囲から抽出
-sneaker_data = [string_intcleaning(value) for value in all_btag_list[:4]]
-gems_data = [string_intcleaning(value) for value in all_btag_list[:4]]
-scroll_data = [string_intcleaning(value) for value in all_btag_list[:4]]
+sneaker_data = [string_intcleaning(value) for value in all_btag_list[:15]]
+sneaker_count_data = [string_intcleaning(value) for value in all_btag_list[16:31]]
+gems_data = [string_intcleaning(value) for value in all_btag_list[32:77]]
+gems_count_data = [string_intcleaning(value) for value in all_btag_list[78:123]]
+scroll_data = [string_intcleaning(value) for value in all_btag_list[124]]        # 修正必要
+scroll_count_data = [string_intcleaning(value) for value in all_btag_list[125]]  # 修正必要
 
 print(sneaker_data)
+print(sneaker_count_data)
 print(gems_data)
+print(gems_count_data)
 print(scroll_data)
+print(scroll_count_data)
